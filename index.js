bc23e5f39a232b2f2d116d5a083794a4fae333f0
wellington-evolution <152021464+wellington-evolution@users.noreply.github.com>
2023-11-28 01:08:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 try {
-  throw new Error("Intentional error");
   const [node, script, input_one] = process.argv;
   const time = new Date().toTimeString();
   const output = `Hello ${input_one}! Right now its ${time}!`;
