f241c04fb1e422e55afe89ef6882e6bb18e2eb0a
wellington-evolution <152021464+wellington-evolution@users.noreply.github.com>
2023-11-27 22:06:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  const nameToGreet = getInput("who-to-greet");
-  console.log("Hello ${nameToGreet}!");
+  const nameToGreet = core.getInput("who-to-greet");
+  console.log(`Hello ${nameToGreet}!`);
 
   const time = new Date().toTimeString();
   core.setOutput("time", time);
