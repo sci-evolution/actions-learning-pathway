2f77cce612b24a1afbdcdd8f7dddbd9f18e5d240
wellington-evolution <152021464+wellington-evolution@users.noreply.github.com>
2023-11-28 00:09:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 try {
   const [node, script, input_one] = process.argv;
-  const time = new Date();
+  const time = new Date().toTimeString();
+  const output = `Hello ${input_one}! Right now its ${time}!`;
   console.log(time);
 } catch(err) {
   console.error(err.message);
